2df917bc3324dfd222afa7df728846b2c95d3ef0
vinceh121 <contact@vinceh121.me>
2025-01-13 23:21:57 +0100

    feat: extract scripts

--- extract-source.js
+++ extract-source.js
@@ -1,28 +1,53 @@
-/**
- * Syntax: node extract.js fr
- */
-
 const { argv, argc, exit } = require("node:process");
 const { readFileSync, writeFileSync } = require("node:fs");
 
 if (argc <= 4) {
-    console.error("Syntax: node extract.js fr");
+    console.error("Syntax: node extract-source.js Japanese fr");
     exit(-1);
 }
 
-const languageCode = argv[2];
+const sourceLanguageName = argv[2];
+const targetLanguageCode = argv[3];
+const placeholder = argv[4];
 
 const translations = JSON.parse(String(readFileSync("Translations.json")));
-const flat = {msg: {}, custom: {}};
+const flat = {
+    msg: {},
+    cmd: {},
+    terms: {},
+    custom: {},
+};
 
 for (const msg in translations.msg) {
-    flat.msg[msg] = "";
+    if (placeholder) {
+        flat.msg[msg] = msg;
+    } else {
+        flat.msg[msg] = "";
+    }
 }
 
-for (const msg in translations.custom["Japanese"]) {
-    flat.custom[msg] = "";
+for (const cmd in translations.cmd) {
+    if (placeholder) {
+        flat.cmd[cmd] = cmd;
+    } else {
+        flat.cmd[cmd] = "";
+    }
 }
 
-// TODO terms, commands, etc
+for (const term in translations.terms) {
+    if (placeholder) {
+        flat.terms[term] = term;
+    } else {
+        flat.terms[term] = "";
+    }
+}
 
-writeFileSync(languageCode + ".json", JSON.stringify(flat, undefined, 4));
+for (const msg in translations.custom[sourceLanguageName]) {
+    if (placeholder) {
+        flat.custom[msg] = msg;
+    } else {
+        flat.custom[msg] = "";
+    }
+}
+
+writeFileSync(targetLanguageCode + ".json", JSON.stringify(flat, undefined, 4));
--- extract.js
+++ extract.js
@@ -14,16 +14,27 @@ const languageName = argv[2];
 const languageCode = argv[3];
 
 const translations = JSON.parse(String(readFileSync("Translations.json")));
-const flat = {msg: {}, custom: {}};
+const flat = {
+    msg: {},
+    cmd: {},
+    terms: {},
+    custom: {},
+};
 
 for (const msg in translations.msg) {
     flat.msg[msg] = translations.msg[msg][languageName];
 }
 
+for (const cmd in translations.cmd) {
+    flat.cmd[cmd] = translations.cmd[cmd][languageName];
+}
+
+for (const term in translations.terms) {
+    flat.terms[term] = translations.terms[term][languageName];
+}
+
 for (const msg in translations.custom[languageName]) {
     flat.custom[msg] = translations.custom[languageName][msg];
 }
-
-// TODO terms, commands, etc
 
 writeFileSync(languageCode + ".json", JSON.stringify(flat, undefined, 4));
