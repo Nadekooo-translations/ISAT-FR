8db534a099a9f12c513a6b58017558bc819a3a98
vinceh121 <contact@vinceh121.me>
2025-08-29 21:33:55 +0200

    feat: patch build script and patch artefact in workflow

--- build-patch.sh
+++ build-patch.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/bash
 
+DIFF=$(diff -qr isat isat-orig | perl -n -e '/Files isat\/([a-zA-Z0-9\/.-_ \[\]]+) and / && print $1."\n"')
 
+mkdir patch
+
+for i in $DIFF
+do
+	cp --parents isat/$i patch
+done
+
+mv patch/isat patch/www
+
+cd patch && zip -r patch.zip * && cd ..
+
+mv patch/patch.zip .
