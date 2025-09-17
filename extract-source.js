d482aca80b9128e3bc1a981a5c68f4d4249518d1
Vincent <vince@iwit-systems.fr>
2025-09-17 14:00:17 +0200

    wip: start working on object string extractions

--- extract-source.js
+++ extract-source.js
@@ -10,12 +10,44 @@ const sourceLanguageName = argv[2];
 const targetLanguageCode = argv[3];
 const placeholder = argv[4];
 
-const translations = JSON.parse(String(readFileSync("Translations.json")));
+const objects = [
+    {
+        file: "Actors",
+        fields: ["name"]
+    },
+    {
+        file: "Armors",
+        fields: ["name", "description"],
+    },
+    {
+        file: "Classes",
+        fields: ["name"],
+    },
+    {
+        file: "Enemies",
+        fields: ["name"],
+    },
+    {
+        file: "Items",
+        fields: ["name", "description"],
+    },
+    {
+        file: "Skills",
+        fields: ["message1", "message2", "name", "description"],
+    },
+    {
+        file: "Weapons",
+        fields: ["name", "description"],
+    },
+];
+
+const translations = JSON.parse(String(readFileSync("isat-orig/data/Translations.json")));
 const flat = {
     msg: {},
     cmd: {},
     terms: {},
     custom: {},
+    objects: {},
 };
 
 for (const msg in translations.msg) {
@@ -47,6 +79,19 @@ for (const msg in translations.custom[sourceLanguageName]) {
         flat.custom[msg] = msg;
     } else {
         flat.custom[msg] = "";
+    }
+}
+
+for (const objectInfo of objects) {
+    const objs = JSON.parse(String(readFileSync(`isat-orig/data/${objectInfo.file}.json`)));
+    flat.objects[objectInfo.file] = {};
+
+    for (const obj of objs) {
+        if (!obj) continue;
+
+        for (const field of objectInfo.fields) {
+            flat.objects[objectInfo.file][field] = obj[field];
+        }
     }
 }
 
