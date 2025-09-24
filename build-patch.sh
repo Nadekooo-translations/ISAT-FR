11fe69f911aec4fcb5c6be48d46ee666d56e6708
Vincent <vince@iwit-systems.fr>
2025-09-24 13:05:45 +0200

    fix: translation plugin patch to translate database from JSON

--- build-patch.sh
+++ build-patch.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+patch isat/js/plugins/SRD_TranslationEngine.js SRD_TranslationEngine.js.patch
+
 DIFF=$(diff -qr isat isat-orig | perl -n -e '/Files isat\/([a-zA-Z0-9\/.-_ \[\]]+) and / && print $1."\n"')
 
 mkdir patch
