99cd37054c30eabf824e755da01df2bb8b21d503
Vincent <vince@iwit-systems.fr>
2026-02-09 14:02:01 +0100

    feat: begin extraction of harcoded strings

--- extract-source.js
+++ extract-source.js
@@ -51,4 +51,32 @@ for (const msg in translations.custom[sourceLanguageName]) {
     }
 }
 
+// EXTRACT TRANSLATIONS IN SCRIPTS
+
+const extractScriptTranslations = (prefix, ops) => {
+    for (let i = 0; i < ops.length; i++) {
+        let op = ops[i];
+
+        if (op.code !== 111 || op.parameters[0] !== 12 || op.parameters[1] !== "ConfigManager.getLanguage() === \"English\"") continue;
+
+        for (; i < ops.length && (op.code !== 111 || op.parameters[0] !== 12 || op.parameters[1] !== "ConfigManager.getLanguage() === \"Japanese\""); i++) {
+            op = ops[i];
+
+            if (op.code === 122 && op.parameters[2] === 0 && op.parameters[3] === 4) {
+                let obj = flat;
+
+                for (const key of prefix) {
+                    obj = obj[key] ??= {};
+                }
+
+                obj[i.toString()] = JSON.stringify(op.parameters[4]); // FIXME ignore those that are actual code
+            }
+        }
+    }
+};
+
+JSON.parse(String(readFileSync("isat-orig/data/CommonEvents.json")))
+        .filter(Boolean)
+        .forEach(e => extractScriptTranslations(['CommonEvents', String(e.id)], e.list))
+
 writeFileSync(targetLanguageCode + ".json", JSON.stringify(flat, undefined, 4));
